342c8e1d54953de171249690d73470af7370a286
Manuel Puchades Bresó <nelopuchadesbreso@gmail.com>
2020-01-11 23:35:37 +0100

    Adding controller with promises and temporary mock

--- components/message/network.js
+++ components/message/network.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const response = require('../../network/response');
+const controller = require('./controller');
 
 router.get('/', function(req, res){
-    console.log(req.headers);
-    res.header({
-        "custom-header": "Nuestro valor personalizado"
-    })
-    response.success(req, res, 'Lista de mensajes');
+
+    controller.getMessages()
+        .then((messageList) => {
+            response.success(req, res, messageList, 200);
+        })
+        .catch(e => {
+            response.error(req, res, 'Unexpected error', 500, e);
+        });
+        
 });
 
 router.post('/', function(req, res){
-    console.log(req.query);
-    if(req.query.error == "ok"){
-        response.error(req, res, 'Error inesperado', 500, 'Es solo una simulación de los errores');
-    } else {
-        response.success(req, res, 'Creado correctamente', 201);
-    }
-});
+    
+    controller.addMessage(req.body.user, req.body.message)
+        .then ((fullMessage) => {
+            response.success(req, res, fullMessage, 201);
+        })
+        .catch (e => {
+            response.error(req, res, 'Información inválida', 400, 'Error en el controlador');
+        });
 
-router.get('/', function(req, res) {
-    response.success(req, res, 'Bienvenido al servidor');
 });
 
 module.exports = router;
