284d7662dc1e8b2e1163479c01e36719dc06a129
Manuel Puchades Bresó <nelopuchadesbreso@gmail.com>
2020-01-12 21:19:16 +0100

    Add config.js

--- network/response.js
+++ network/response.js
@@ -1,15 +1,44 @@
+const statusMessages = {
+    '200': 'Done',
+    '201': 'Created',
+    '400': 'Invalid format',
+    '500': 'Internal error'
+}
+
 exports.success = function (req, res, message, status) {
-    res.status(status || 200).send({
+
+    let statusCode = status;
+    let statusMessage = message;
+
+    if(!status){
+        status = 200;
+    }
+
+    if(!message){
+        statusMessage = statusMessages[status];
+    }
+
+    res.status(statusCode).send({
         error: '',
-        body: message
+        body: statusMessage
     });
 }
 
 exports.error = function (req, res, message, status, details) {
-    console.error('[response error] ' + details);
+    
+    let statusCode = status;
+    let statusMessage = message;
 
-    res.status(status || 500).send({
-       error: message,
+    if(!status){
+        status = 500;
+    }
+
+    if(!message){
+        statusMessages = statusMessages[status];
+    }
+
+    res.status(statusCode).send({
+       error: statusMessage,
        body: '' 
     });
 }
--- server.js
+++ server.js
@@ -2,21 +2,25 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+const config = require('./config');
+
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const socket = require('./socket');
 const db = require('./db');
 const router = require('./network/routes');
 
-db('mongodb+srv://db_user:4W2vv9SluC2Pms3D@cluster0-vdlpx.mongodb.net/test?retryWrites=true&w=majority');
+db(config.dbUrl);
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 socket.connect(server);
 router(app);
 
-app.use('/app', express.static('public'));
+app.use('/' + config.publicRoute, express.static('public'));
 
-server.listen(3000, function() {
-    console.log('La aplicación está escuchando en http://localhost:3000');
+server.listen(config.port, function() {
+    console.log('La aplicación está escuchando en ' + config.host + ':' + config.port);
 });
